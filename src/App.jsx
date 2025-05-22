eaf830eba6e56dedecf14cee3e73b3e7d7e8f79f
MohdSaquib114 <msmsaq114@gmail.com>
2025-05-22 17:00:56 +0530

    Init: Initialize react vite, add react router, tailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+
+function App() {
+  return (
+    // <Routes>
+    //   <Route path="/" element={<Home />} />
+    //   <Route path="/about" element={<About />} />
+    // </Routes>
+    <div className="text-red-600">
+      dfdfdfd
+    </div>
+  );
+}
+
+export default App;
